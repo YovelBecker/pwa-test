f77be5f142691301ffff353c3fd681b0b1eb2c5f
Yovel Becker <yovelb13@gmail.com>
2019-08-04 10:48:50 +0300

    Updates

---
+++ precache-manifest.1f1f90f7e573cbc00d11bf1f5ad676de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14a527eb29b2b454edaf6a263f5ec5a",
+    "url": "/pwa-test/index.html"
+  },
+  {
+    "revision": "e224af7a8cbba4758431",
+    "url": "/pwa-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pwa-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e224af7a8cbba4758431",
+    "url": "/pwa-test/static/js/main.f23e8251.chunk.js"
+  },
+  {
+    "revision": "5cf7397ead97da4fa197",
+    "url": "/pwa-test/static/js/runtime~main.1b8f209f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
